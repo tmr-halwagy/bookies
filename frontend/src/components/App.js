1b1ab4dff6136ad10fa49f5d2e26d21ff92a177f
FH-N <faridanasrat@gmail.com>
2024-11-29 00:55:42 +0200

    changed to modern react

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,19 +1,14 @@
-import React, { Component } from "react";
-import { render } from "react-dom";
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Test</h1>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  );
+};
 
 const appDiv = document.getElementById("app");
-render(<App />, appDiv);
+const root = createRoot(appDiv);
+root.render(<App />);
